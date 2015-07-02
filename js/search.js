81e3bb855b9e4e506041ed0fea176df0bf03878f
Juan Pablo Mejia Duque <juanpmd@hotmail.com>
2015-07-01 22:27:42 -0500

    v0.01a
    - En esta version agregue las imagenes en vista previa de cada video.
    - Agregue el uso de la tecla enter para buscar un video
    - Modifique el css para que ajustara el fondo de acuerdo a los videos
    desplegados en la lista

--- js/search.js
+++ js/search.js
@@ -7,9 +7,23 @@ function init() {
 // ##################################################################################
 function onYouTubeApiLoad() {
     gapi.client.setApiKey('AIzaSyAgifQmVZ7pWXjYJ4rZLITviMH39pFPA1g');
+
+    //Agregue esta variable para que cada vez que unda el click el tamaño del fondo se resetee
+    var sizebackground =parseInt($(".results").css("height"), 10);
+
     $("#boton").click(function(){
+      $(".results").css("height", sizebackground);
       search(); //Aqui hace la busqueda
     });
+
+    //Esta funcion es para que no tenga que dar click sino undir la tecla "enter"
+    $("#search").keypress(function (e) {
+      if (e.which == 13) {
+        $(".results").css("height", sizebackground);
+        search(); //Aqui hace la busqueda
+      }
+    });
+
 }
 // ##################################################################################
 function search() {
@@ -28,10 +42,18 @@ function search() {
 function onSearchResponse(response) {
     var results = response.items;
 
+    //utilizo esto por si ya habia undido el boton entonces borre la lista que ya esta mostrando
+    $(".video-lista").remove();
+
+    //Esto es para que cuando se unda buscar, aparezca el bloque donde estara la lista de videos
+    $(".results").removeClass("hidden");
+
     //Esto es para cambiar color de boton boton-list
     $("#boton-list").addClass("boton-list-class2");
     $("#boton-list").removeClass("boton-list-class");
 
+    //la var temporal es para darle un numero al id unico de cada elemento de la lista de resultados
+    var temporal = 0;
     for (result in results){
       var tamartista = results[result].snippet.channelTitle.length;
       if (tamartista === 0){}
@@ -43,13 +65,27 @@ function onSearchResponse(response) {
         var sizebackground =parseInt($(".results").css("height"), 10);
         $(".results").css("height", sizebackground + 71);
 
-        var vistaboton = '<div class="menu-videos"><div class="video-lista"><div class="play"></div><div class="info-video"><p class="nombre-cancion">%nombre-cancion%</p><p class="descripcion-cancion"><span>de </span>%nombre-artista%</p></div></div>';
+        var vistaboton = '<div class="video-lista"><div id="%id-video%" class="video-image"></div><div class="play"></div><div class="info-video"><p class="nombre-cancion">%nombre-cancion%</p><p class="descripcion-cancion"><span>de </span>%nombre-artista%</p></div></div>';
         vistaboton = vistaboton.replace("%nombre-cancion%", titulofinal);
         vistaboton = vistaboton.replace("%nombre-artista%", results[result].snippet.channelTitle);
+
+        var strnum = String(temporal);
+        strnum = "video" + strnum;
+        vistaboton = vistaboton.replace("%id-video%", strnum);
         $(".results").append(vistaboton);
+
+        //el strnum es para crear un id unico a cada div, luego a cada id le agrego si background para que asi no se repita
+        var imagenvideo = 'url(' + results[result].snippet.thumbnails.medium.url + ')';
+        var pos = "#" + strnum;
+        $(pos).css('background-image', imagenvideo);
       }
+      temporal = temporal+1;
       //var plantilla ='<iframe width="640" height="360" src="http://www.youtube.com/embed/%idvideo%" frameborder="0" allowfullscreen></iframe>';
     };
+
+    //Esto es para organizar el tamaño del fondo gris (que se organice con la cantidad de resultados de la busqueda)
+    sizebackground =parseInt($(".results").css("height"), 10);
+    $(".main-results").css("height", sizebackground + 50);
 }
 // ##################################################################################
 function TamanoModificado(titulo){
